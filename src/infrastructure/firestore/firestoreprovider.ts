ab1e59ef126ac06f2218d62dd56bd00fef8f5697
LeonardoVinagre <lvinagre2.0@gmail.com>
2022-11-09 10:25:08 -0300

    Enable cors

--- src/infrastructure/firestore/firestoreprovider.ts
+++ src/infrastructure/firestore/firestoreprovider.ts
@@ -1,6 +1,5 @@
 var admin = require("firebase-admin");
-// const serviceAccount = require('../firestore/ServiceAccountKey.json');
-// console.log(serviceAccount);
+
 admin.initializeApp({
     credential: admin.credential.cert('./src/infrastructure/firestore/ServiceAccountKey.json'),
     databaseURL: 'https://pdv-eep-7469a-default-rtdb.firebaseio.com'
